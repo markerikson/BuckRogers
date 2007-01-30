1c52baca55570eaa01790fe0caba0ffc8cd2ce38
Mark Erikson <mark@isquaredsoftware.com>
2007-01-30 18:25:23 +0000

    Forgot to bump the release to 0.7.8.

--- Interface/Other/AssemblyInfo.cs
+++ Interface/Other/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.7.7.602")]
-[assembly: AssemblyFileVersion("0.7.7.602")]
+[assembly: AssemblyVersion("0.7.8.603")]
+[assembly: AssemblyFileVersion("0.7.8.603")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
